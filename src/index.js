6ebfde3ee65491f8928fd786dcc86dc54f9b0711
Chotiwut Sattayapisan <ezio.magtap@gmail.com>
2020-05-29 14:25:06 +0700

    create constructor function to initialize state
    state must be in object

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+
+        this.stae = { lat: null };
+    }
+
+
+
     render() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => console.log(position),
