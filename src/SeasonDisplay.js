2e6d51cbb36cd8b59f53dec75d6e9bacb915f1bb
Chotiwut Sattayapisan <ezio.magtap@gmail.com>
2020-05-30 13:21:20 +0700

    create season display result
    add cases about season by month

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9){
+        lat > 0 ? 'summer' : 'winter';
+    } else {
+        lat > 0 ? 'winter' : 'summer';
+    }
+}
+
+
 const SeasonDisplay = (props) => {
-    
+    const season = getSeason(props.lat, new Date().getMonth());
+
     return (
         <div>Season Display</div>
     );
